1fb98e2e21e514cf3199a1c4945167751d8837a0
Leandro Rocha <prog.leandro@gmail.com>
2019-08-14 23:01:17 -0300

    Correção

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,97 +1,102 @@
-import React, { useEffect, useState} from 'react';
-import io from 'socket.io-client';
-import './Main.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./Main.css";
+import { Link } from "react-router-dom";
 
-import api from '../services/api';
+import api from "../services/api";
 
-import itsamatch from '../assets/itsamatch.png';
-import logo from '../assets/logo.svg';
-import like from '../assets/like.svg';
-import dislike from '../assets/dislike.svg';
+import itsamatch from "../assets/itsamatch.png";
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
 
-export default function Main({ match }){
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
-    const [users, setUsers] = useState([]);
-    const [matchDev, setMatchDev] = useState(null);
-
-    useEffect(()=> {
-        async function loadUsers(){
-            const response = await api.get('/devs', {
-                headers:{
-                    user: match.params.id
-                }
-            })
-
-            setUsers(response.data);
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
         }
-        loadUsers();
-    }, [match.params.id]);
-
-    useEffect(()=> {
-      const socket = io('http://localhost:3333', {
-          query: { user: match.params.id}
       });
 
-      socket.on('match', dev => {
-          setMatchDev(dev);
-      });
-      
-    }, [match.params.id]);
-
-    async function handleLike(id){
-        await api.post(`/devs/${id}/like`, null, {
-            headers: {user: match.params.id}
-        });
-
-        setUsers(users.filter(user => user._id !== id));
+      setUsers(response.data);
     }
-    async function handleDislike(id){
-        await api.post(`/devs/${id}/dislike`, null, {
-            headers: {user: match.params.id}
-        });
+    loadUsers();
+  }, [match.params.id]);
 
-        setUsers(users.filter(user => user._id !== id));
-    }
+  useEffect(() => {
+    const socket = io("//leandrorocha.dev/api/timdev/", {
+      query: { user: match.params.id }
+    });
 
-    return (
-        <div className="main-container">
-            <Link to="/">
-                <img src={logo} alt="Tindev"/>
-            </Link>
-            { users.length > 0 ? (
-                <ul>
-                    {users.map(user => (
-                        <li key={user._id}>
-                            <img src={user.avatar} alt={user.name} />
-                            <footer>
-                                <strong>{user.name}</strong>
-                                <p>{user.bio}</p>
-                            </footer>
-                            <div className="buttons">
-                                <button type="button" onClick={() => handleDislike(user._id)}>
-                                    <img src={dislike} alt="Dislike"></img>
-                                </button>
-                                <button type="button" onClick={() => handleLike(user._id)} >
-                                <img src={like  } alt="Like"></img>
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <div className="empty"> Acabou :(</div>
-            )}
+    socket.on("match", dev => {
+      setMatchDev(dev);
+    });
+  }, [match.params.id]);
 
-            { matchDev && (
-                <div className="match-container">
-                    <img src={itsamatch} alt="Its a match" />
-                    <img className="avatar" src={matchDev.avatar}  alt=""/>
-                    <strong>{matchDev.name}</strong>
-                    <p> {matchDev.bio}</p>
-                    <button type="button" onClick={() => { setMatchDev(null)}} >FECHAR</button>
-                </div>
-            )}
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/like`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislike`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty"> Acabou :(</div>
+      )}
+
+      {matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="Its a match" />
+          <img className="avatar" src={matchDev.avatar} alt="" />
+          <strong>{matchDev.name}</strong>
+          <p> {matchDev.bio}</p>
+          <button
+            type="button"
+            onClick={() => {
+              setMatchDev(null);
+            }}
+          >
+            FECHAR
+          </button>
         </div>
-    )
+      )}
+    </div>
+  );
 }
